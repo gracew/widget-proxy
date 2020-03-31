d0ba8386a943565c4d6f3585786ecb03eae80ea5
Grace Wang <gracewang92@gmail.com>
2020-03-31 13:14:34 -0700

    actually document how this interacts with the widget launcher

--- README.md
+++ README.md
@@ -1,11 +1,35 @@
 # widget-proxy
 
-## Build docker image
+## Base API server
 
-```
-docker build . -t widget-proxy
-```
+This repository contains the code and docker image for user-defined API servers.
+
+Expected usage:
+
+- A docker image with the source and dependencies already installed is defined via `Dockerfile.src`. This image can be
+  built by running:
 
 ```
 docker build . -f Dockerfile.src -t widget-proxy-src
 ```
+
+- The resulting image is expected to be used as a base for building the runtime image (containing a go binary). When
+  building the runtime image the file `generated/model.go` should be replaced by a file corresponding to the user-defined
+  API.
+- The runtime image is expected to be launched with the following inputs:
+  - environment variable `API_NAME`
+  - file containing the API definition at `/app/api.json`
+  - file containing the auth definition at `/app/auth.json`
+  - file containing the custom logic definition at `/app/customLogic.json`
+
+Based on the custom logic definition, the API server will make requests to a custom logic server at
+`http://custom-logic:8080`. All requests are POST requests. Paths are expected to be of the form
+`/{when}{operation}`, for example `/beforecreate`, `/afterdelete`, or `/beforemarkComplete` for an update action
+named `markComplete`.
+
+## Custom logic
+
+This repository also contains the docker images for running custom logic, found in the `docker/` directory. These images
+are expected to be launched with a directory at `/app/customLogic` containing user-specified custom logic. Each file
+in this directory corresponds to a POST HTTP endpoint, where the filename (with extension stripped) is the endpoint
+path. For example, a file `beforecreate.js` or `beforecreate.py` would result in the endpoint `/beforecreate`.
--- docker/docker_integration_test.go
+++ docker/docker_integration_test.go
@@ -12,16 +12,6 @@ import (
 
 // not really a test, but it spins up a node-runner instance that can be manually interacted with
 func TestNode(t *testing.T) {
-	t.Skip()
-	buildCmd := exec.Command("docker",
-		"build",
-		"node",
-		"-t",
-		"node-runner",
-	)
-	err := buildCmd.Run()
-	assert.NoError(t, err)
-
 	beforeCreate := `
 function beforeSave(input) {
 	input.concat = input.name + " " + input.score;
@@ -31,32 +21,10 @@ function beforeSave(input) {
 module.exports = beforeSave;
 `
 
-	customLogicDir, err := ioutil.TempDir("/Users/gracew/tmp", "customLogic-")
-	assert.NoError(t, err)
-
-	err = writeFileInDir(customLogicDir, "beforeCreate.js", beforeCreate)
-	assert.NoError(t, err)
-
-	containerName := uuid.New().String()
-	runCmd := exec.Command("docker",
-		"run",
-		"-d",
-		"-v",
-		customLogicDir+":/app/customLogic",
-		"-p",
-		"7070:8080",
-		"--name",
-		containerName,
-		"--network",
-		"widget-proxy_default",
-		"node-runner",
-	)
-	out, err := runCmd.CombinedOutput()
-	assert.NoError(t, err, "%s", string(out))
+	setupContainer(t, "node-runner", []fileContent{fileContent{filename: "beforecreate.js", content: beforeCreate}})
 }
 
 func TestPython(t *testing.T) {
-	t.Skip()
 	buildCmd := exec.Command("docker",
 		"build",
 		"python",
@@ -90,6 +58,48 @@ def beforeCreate(input):
 		"--network",
 		"widget-proxy_default",
 		"python-runner",
+	)
+	out, err := runCmd.CombinedOutput()
+	assert.NoError(t, err, "%s", string(out))
+}
+
+type fileContent struct {
+	filename string
+	content string
+}
+
+// returns the container name
+func setupContainer(t *testing.T, buildPath string, fileContents []fileContent) string {
+	buildCmd := exec.Command("docker",
+		"build",
+		buildPath,
+		"-t",
+		buildPath + "-runner",
+	)
+	err := buildCmd.Run()
+	assert.NoError(t, err)
+
+	customLogicDir, err := ioutil.TempDir("/Users/gracew/tmp", "customLogic-")
+	assert.NoError(t, err)
+
+	for _, fileContent := range fileContents {
+		err = writeFileInDir(customLogicDir, fileContent.filename, fileContent.content)
+		assert.NoError(t, err)
+	}
+
+	containerName := uuid.New().String()
+	runCmd := exec.Command("docker",
+		"run",
+		"-d",
+		"-v",
+		customLogicDir+":/app/customLogic",
+		"-p",
+		"7070:8080",
+		"--name",
+		containerName,
+		"--network",
+		"widget-proxy_default",
+		buildPath,
 	)
 	out, err := runCmd.CombinedOutput()
 	assert.NoError(t, err, "%s", string(out))
